cf9ce344cbe3ca8b50d96d0693bfab352497ba17
xogh3198 <taeho3198@naver.com>
2026-07-30 17:31:26 +0900

    fix: expose briefing guides and tools

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      '/briefing',
-      '/calendar',
-      '/dividend',
-      '/guides',
-      '/methodology',
-      '/market',
-      '/search',
-      '/tools',
-    ].map((source) => ({
-      source: `${source}/:path*`,
-      destination: '/forum',
-      permanent: false,
-    }));
-  },
 };
 
 module.exports = nextConfig;
